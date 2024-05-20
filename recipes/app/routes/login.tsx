74430d40402aafa896fb7b37ab14ee52e572a2dc
Tinh Tran <tttinh@sk-global.biz>
2024-05-20 09:35:52 +0700

    feat: Add remix menu app Tailwind CSS configuration and files

---
+++ recipes/app/routes/login.tsx
@@ -0,0 +1,85 @@
+import {
+  type ActionFunctionArgs,
+  type LoaderFunctionArgs,
+  json,
+} from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
+import { z } from "zod";
+import { ErrorMessage, PrimaryButton, PrimaryInput } from "~/components/forms";
+import { generateMagicLink, sendMagicLinkEmail } from "~/magic-links.server";
+import { commitSession, getSession } from "~/sessions";
+import { validateForm } from "~/utils/validation";
+import { v4 as uuid } from "uuid";
+import { requireLoggedOutUser } from "~/utils/auth.server";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  await requireLoggedOutUser(request);
+  return null;
+}
+
+const loginSchema = z.object({
+  email: z.string().email(),
+});
+
+export async function action({ request }: ActionFunctionArgs) {
+  await requireLoggedOutUser(request);
+
+  const cookieHeader = request.headers.get("cookie");
+  const session = await getSession(cookieHeader);
+  const formData = await request.formData();
+
+  return validateForm(
+    formData,
+    loginSchema,
+    async ({ email }) => {
+      const nonce = uuid();
+      session.set("nonce", nonce);
+
+      const link = generateMagicLink(email, nonce);
+      await sendMagicLinkEmail(link, email);
+
+      return json(
+        { success: true },
+        {
+          headers: {
+            "Set-Cookie": await commitSession(session),
+          },
+        }
+      );
+    },
+    (errors) => json({ errors, email: formData.get("email") }, { status: 400 })
+  );
+}
+
+export default function Login() {
+  const actionData = useActionData<any>();
+  return (
+    <div className="text-center mt-36">
+      {actionData?.success ? (
+        <div>
+          <h1 className="text-2xl py-8">Yum!</h1>
+          <p>
+            Check your email and follow the instructions to finish logging in.
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h1 className="text-3xl mb-8">Remix Recipes</h1>
+          <form method="post" className="mx-auto md:w-1/3">
+            <div className="text-left pb-4">
+              <PrimaryInput
+                type="email"
+                name="email"
+                placeholder="Email"
+                autoComplete="off"
+                defaultValue={actionData?.email}
+              />
+              <ErrorMessage>{actionData?.errors?.email}</ErrorMessage>
+            </div>
+            <PrimaryButton className="w-1/3 mx-auto">Log In</PrimaryButton>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
